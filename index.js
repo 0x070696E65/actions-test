03eb9c1bc515516aef2f3d34464edd8752300e7f
toshiya matsukawa <matsukawa5955@gmail.com>
2022-07-26 16:56:16 +0300

    test

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const symbol = require('symbol-sdk');
 const fs = require('fs');
 require('dotenv').config();
 
@@ -26,7 +27,57 @@ try {
   // `who-to-greet` input defined in action metadata file
   const nameToGreet = core.getInput('who-to-greet');
   const BOT_PRIVATEKEY = core.getInput('BOT_PRIVATEKEY');
-  console.log(`Hello ${BOT_PRIVATEKEY}!`);
+
+  const NODE = "https://hideyoshi.mydns.jp:3001";
+const wsEndpoint = NODE.replace('http', 'ws') + "/ws";
+const nsHttp = new symbol.NamespaceHttp(NODE);
+const listener = new symbol.Listener(wsEndpoint,nsHttp,WebSocket);
+const transactionService = new symbol.TransactionService(txRepo, receiptHttp);
+var ea = 1637848847;
+var nt =symbol.NetworkType.TEST_NET;
+const ng = "7FCCD304802016BEBBCD342A332F91FF1F3BB5E902988B352697BE245F48E836";
+const deadline = symbol.Deadline.create(ea);
+const bene = symbol.PublicAccount.createFromPublicKey("71754759FD4F25981ED20F60050C20AB1E7CA104A87EC758E9B1E69FCA0286D6", nt);
+const bot = symbol.Account.createFromPrivateKey(BOT_PRIVATEKEY, nt);
+const bob = symbol.PublicAccount.createFromPublicKey("B055C6F655CD3101A04567F9499F24BE7AB970C879887BD3C6644AB7CAA22D22", nt);
+
+const tx = symbol.TransferTransaction.create(
+    deadline,
+    bob.address,
+    [new symbol.Mosaic(new symbol.MosaicId("3A8416DB2D53B6C8"), symbol.UInt64.fromUint(1000000))],
+    symbol.PlainMessage.create("test"),
+    nt
+  ).setMaxFee(100)
+  
+  const agg = symbol.AggregateTransaction.createBonded(
+    deadline,
+    [tx.toAggregate(bene)],
+    nt
+  ).setMaxFeeForAggregate(100, 2)
+  
+  const signedAggregateTx = bot.sign(agg, ng);
+  
+  const hashLockTx = symbol.HashLockTransaction.create(
+      deadline,
+      new symbol.Mosaic(new symbol.MosaicId("3A8416DB2D53B6C8"), symbol.UInt64.fromUint(10000000)),
+      symbol.UInt64.fromUint(480),
+      signedAggregateTx,
+      nt,
+      symbol.UInt64.fromUint(1000000)
+  );
+  
+  const signedLockTx = bot.sign(hashLockTx, ng);
+  listener.open().then(() => {
+      transactionService.announceHashLockAggregateBonded(
+        signedLockTx,
+        signedAggregateTx,
+        listener
+      ).subscribe(aggTx => {
+          console.log(aggTx)
+          listener.close();
+      })
+  });
+
   console.log(`Hello ${nameToGreet}!`);
   const time = (new Date()).toTimeString();
   const address = process.env.ADDRESS;
