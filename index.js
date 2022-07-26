d83d48fbe799b90f5315217d19d81ef72464c144
toshiya matsukawa <matsukawa5955@gmail.com>
2022-07-26 17:33:52 +0300

    test

--- index.js
+++ index.js
@@ -107,7 +107,6 @@ const tx = symbol.TransferTransaction.create(
   });
   */
 
-
   console.log(`Hello ${nameToGreet}!`);
   const time = (new Date()).toTimeString();
   const address = process.env.ADDRESS;
