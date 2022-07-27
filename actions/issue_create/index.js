7b8b6c56015e273a817a9f5d4e766b0b7a992ba7
toshiya matsukawa <matsukawa5955@gmail.com>
2022-07-27 10:14:47 +0300

    test

--- actions/issue_create/index.js
+++ actions/issue_create/index.js
@@ -3,12 +3,28 @@ const github = require('@actions/github');
 
 require('dotenv').config();
 
+function getValue(data, ward){
+    const lines = data.split("\n");
+    console.log(lines);
+
+    function filterWards(arr, query) {
+        return arr.filter(function(el) {
+            return el.indexOf(query) !== -1;
+        });
+    }
+    const filterd = filterWards(lines, ward);
+    return filterd[0].replace(ward, "");
+}
+
 try {
   // `who-to-greet` input defined in action metadata file
   const payload = JSON.stringify(github.context.payload, undefined, 2)
   console.log(`The event payload: ${payload}`);
   console.log(github.context.payload.issue.title);
   console.log(github.context.payload.issue.body);
+  const comment = github.context.payload.issue.body;
+  console.log(getValue(comment, "#reward="))
+  console.log(getValue(comment, "#address="))
 } catch (error) {
   core.setFailed(error.message);
 }
