b2dcc649e1488b9f67bd3f57d302f710412f76e4
toshiya matsukawa <matsukawa5955@gmail.com>
2022-07-28 13:42:05 +0300

    test

--- actions/issue_assigned/index.js
+++ actions/issue_assigned/index.js
@@ -3,21 +3,50 @@ const github = require('@actions/github');
 
 require('dotenv').config();
 
-function getValue(data, ward){
-    const lines = data.split("\n");
-    function filterWards(arr, query) {
-        return arr.filter(function(el) {
-            return el.indexOf(query) !== -1;
-        });
-    }
-    const filterd = filterWards(lines, ward);
-    return filterd[0].replace(ward, "");
-}
-
 try {
-  // `who-to-greet` input defined in action metadata file
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
+  const issue = github.context.payload.issue;
+  const assigneeId = issue.assignee.login;
+  console.log(assigneeId);
+  const issue_number = issue.number;
+  console.log(issue_number);
+
+  const resAuth = await axios
+    .post('http://localhost:1337/api/auth/local', {
+      identifier: 'matsukawa5955+bot@gmail.com',
+      password: 'Bot1234567890',
+    })
+  console.log('User profile', resAuth.data.user);
+  const token = resAuth.data.jwt;
+  console.log('User token', token);
+
+  const resUser = await axios
+    .get('http://localhost:1337/api/users');
+
+  const users = resUser.data;
+  console.log(users);
+
+  const user = users.find((d) => d.githubId === assigneeId);
+  console.log(user);
+  const data = {
+    "data": {
+      "githubId": assigneeId,
+      "symbolAddress": user.symbolAddress,
+    }
+  }
+
+  const resReward = await axios
+    .get('http://localhost:1337/api/rewards');
+  const rewards = resReward.data.data;
+  const reward = rewards.find((d) => d.attributes.issueNumber === issueNumber);
+  console.log(reward.id);
+
+  const resUpdateReward = await axios
+    .put('http://localhost:1337/api/rewards/' + reward.id, data, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      }
+    })
+  console.log('Data: ', resUpdateReward.data);
 } catch (error) {
   core.setFailed(error.message);
 }
--- actions/issue_opend/index.js
+++ actions/issue_opend/index.js
@@ -1,26 +1,65 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const axios = require('axios');
 
 require('dotenv').config();
 
-function getValue(data, ward){
-    const lines = data.split("\n");
-    function filterWards(arr, query) {
-        return arr.filter(function(el) {
-            return el.indexOf(query) !== -1;
-        });
-    }
-    const filterd = filterWards(lines, ward);
-    return filterd[0].replace(ward, "");
+function getValue(data, ward) {
+  const lines = data.split("\n");
+  function filterWards(arr, query) {
+    return arr.filter(function (el) {
+      return el.indexOf(query) !== -1;
+    });
+  }
+  const filterd = filterWards(lines, ward);
+  return filterd[0].replace(ward, "");
 }
 
 try {
-  // `who-to-greet` input defined in action metadata file
-  const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
-  const comment = github.context.payload.issue.body;
-  console.log(getValue(comment, "#reward="))
-  console.log(getValue(comment, "#address="))
+  axios
+    .post('http://localhost:1337/api/auth/local', {
+      identifier: 'matsukawa5955+bot@gmail.com',
+      password: 'Bot1234567890',
+    })
+    .then((response) => {
+      console.log('Well done!');
+      console.log('User profile', response.data.user);
+      console.log('User token', response.data.jwt);
+
+      const issue = github.context.payload.issue;
+      const title = issue.title;
+      const issue_url = issue.html_url;
+      const comment = issue.body;
+      const reward_amount = getValue(comment, "#reward=")
+
+      console.log(github.context.payload);
+
+      const data = {
+        "data": {
+          "issueTitle": title,
+          "issueNumber": 0,
+          "issueUrl": issue_url,
+          "rewardType": 0,
+          "rewardAmount": reward_amount,
+        }
+      }
+
+      axios
+        .post('http://localhost:1337/api/rewards', data, {
+          headers: {
+            Authorization: `Bearer ${response.data.jwt}`,
+          }
+        })
+        .then((response) => {
+          console.log('Data: ', response.data);
+        })
+        .catch((error) => {
+          console.log('An error occurred:', error.response);
+        });
+    })
+    .catch((error) => {
+      console.log('An error occurred:', error.response);
+    });
 } catch (error) {
   core.setFailed(error.message);
 }
