09671c2edec650cf3da7e496016d0de6ff455f24
toshiya matsukawa <matsukawa5955@gmail.com>
2022-07-29 13:15:21 +0300

    test

--- actions/issue_connected/index.js
+++ actions/issue_connected/index.js
@@ -1,7 +1,7 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
 const axios = require('axios');
-const api_url = "";
+//const api_url = "";
 
 try {
   console.log(github.context.payload);
