474e9e01d48efcdaa282082520b87cc66db8ba9b
toshiya matsukawa <matsukawa5955@gmail.com>
2022-08-18 17:08:23 +0300

    test

--- actions/task_completed/dist/index.js
+++ actions/task_completed/dist/index.js
@@ -14976,8 +14976,9 @@ const { api_url } = __nccwpck_require__(5405);
 try {
     const branchName = github.context.payload.pull_request.head.ref;
     const assigneeId = github.context.payload.sender.login;
-    console.log(github.context.payload.sender.login)
+    console.log(github.context.payload)
 
+    /*
     axios
         .post(api_url + '/api/auth/local', {
             identifier: process.env.BOT_ID,
@@ -15024,6 +15025,7 @@ try {
             console.error("C: " + error.message);
             throw error;
         });
+        */
 } catch (error) {
     core.setFailed("D: " + error.message);
 }
